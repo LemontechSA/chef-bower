e711573048004e54d8280d17fabc6c7f5e74f10d
Abraham Barrera <abarrerac@gmail.com>
2014-07-02 12:12:06 -0400

    First commit

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default[:npm][:version] = "1.3.11"
+default[:nodejs][:install_method] = 'package'
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'bower'
+maintainer       'oke-ya.inc'
+maintainer_email 'yalab@oke-ya.com'
+license          'MIT'
+description      'Installs/Configures bower'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          "npm"
